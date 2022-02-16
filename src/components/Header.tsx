ecd2aa0aa0ff9fe0929421bb1230a071bc793e7f
Jucemar Dimon <jucemar.dimon@gmail.com>
2022-02-16 09:44:34 -0300

    Conclui a criação do header da aplicação

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
-import { Text, Flex, Input, Icon } from "@chakra-ui/react";
-import { RiSearchLine } from "react-icons/ri";
+import { Text, Flex, Input, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+import {
+  RiSearchLine,
+  RiNotificationLine,
+  RiUserAddLine,
+} from "react-icons/ri";
 
 export function Header() {
   return (
@@ -41,6 +45,29 @@ export function Header() {
           mr="4"
         />
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20"></Icon>
+          <Icon as={RiUserAddLine} fontSize="20"></Icon>
+        </HStack>
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Jucemar Dimon</Text>
+            <Text color="gray.300" fontSize="small">
+              jucemar.dimon@gmail.com
+            </Text>
+          </Box>
+          <Avatar size="md" name="Jucemar Dimon"></Avatar>
+        </Flex>
       </Flex>
     </Flex>
   );
